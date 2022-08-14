e240ab646ed095f341ff1f229c7844542556bfdb
Pablo Embarach <pabloembarach@gmail.com>
2022-08-14 11:08:09 +0000

    Add a game over or play again function

--- run.py
+++ run.py
@@ -12,19 +12,23 @@ print("4 5 6")
 print("7 8 9")
 print("\n")
 
-break_line = [2,5,8]
+break_line = [2, 5, 8]
+
 
 def print_grid(grid):
     """
-    This function is a function that creates the number of spaces and formats the grid.    
+    This function is a function that creates
+    the number of spaces and formats the grid.
     """
     print("\nGrid Status\n")
-    for index in range (len(grid)):
+    for index in range(len(grid)):
         print(grid[index], end=" ")
-        if index == break_line[0] or index == break_line[1] or index == break_line[2]:
+        if index == break_line[0] or index == break_line[1]
+        or index == break_line[2]:
             print (" ")
-            
-grid = ["_"] * 9 # Set number of spaces in the grid
+
+grid = ["_"] * 9  # Set number of spaces in the grid
+
 
 def check_winner(grid, player):
     """
@@ -79,60 +83,100 @@ def check_winner(grid, player):
 
     return 0
 
-number_of_moves = 0 # Variable of number of moves used to not let players exceed the number of valid moves.
 
-while True: 
-    """
-    Game logic, request for Player input, generate a random choice for the CPU, 
-    print the symbol at the chosen location and update the grid.
-    """
+def reset_grid():
+    '''
+    Resets the board if user wants to play again
+    '''
+    grid.clear()
+    grid.extend(["_"] * 9)
+
+
+def return_to_first_page():
+    '''
+    Asks the user if they want to play again or quit when the game ends
+    '''
+    print("Game Ended...\n")
+
+    print("Enter '1' to play again.")
+    print('Enter "Q" if you want to quit the game.')
     while True:
-        try:
+        make_a_choice = input().strip()
+        if make_a_choice.lower() == 'q':
+            print(f'Thank for playing the game.')
+            quit()
+        elif make_a_choice == '1':
+            print(f'Welcome again.')
+            reset_grid()
+            play_game()
 
-            choice = int(input("\nPlayer, make your choice "))
+        else:
+            print("Invalid selection. Please select '1' or 'q'")
 
-            if choice in range(1,10):
-                if grid[choice - 1] == "_":
-                    grid[choice - 1] = choice - 1
-                    break
+
+def play_game():
+
+    number_of_moves = 0  # Variable of number of moves used to not let players
+    # exceed the number of valid moves.
+
+    while True:
+        """
+        Game logic, request for Player input,
+        generate a random choice for the CPU,
+        print the symbol at the chosen location and update the grid.
+        """
+        while True:
+            try:
+
+                choice = int(input("\nPlayer, make your choice "))
+
+                if choice in range(1, 10):
+                    if grid[choice - 1] == "_":
+                        grid[choice - 1] = choice - 1
+                        break
+                    else:
+                        print("\nCheck the Grid. Invalid option!\n")
+                        print_grid(grid)
                 else:
                     print("\nCheck the Grid. Invalid option!\n")
                     print_grid(grid)
-            else:
+            except ValueError:
                 print("\nCheck the Grid. Invalid option!\n")
-                print_grid(grid)
-        except ValueError:
-            print("\nCheck the Grid. Invalid option!\n")
 
+        grid[choice - 1] = "X"
+        number_of_moves += 1
+        winner = check_winner(grid, "X")
+        if winner != 0:
+            break
 
-    grid[choice - 1] = "X"
-    number_of_moves += 1
-    winner = check_winner(grid, "X")
-    if winner !=0:
-        break
+        if number_of_moves == 9:
+            break
+        print_grid(grid)
 
-    if number_of_moves == 9:
-        break
-    print_grid(grid)
-
-    computer_choice = random.randint(1, 9)
-    while grid[computer_choice -1] != "_":
         computer_choice = random.randint(1, 9)
+        while grid[computer_choice - 1] != "_":
+            computer_choice = random.randint(1, 9)
 
-    grid[computer_choice - 1] = "O"
-    number_of_moves += 1
-    
-    winner = check_winner(grid, "O")
-    if winner != 0:
-        break
+        grid[computer_choice - 1] = "O"
+        number_of_moves += 1
+        print_grid(grid)
+
+        winner = check_winner(grid, "O")
+        if winner != 0:
+            break
+
+        print_grid(grid)
+
+    if winner == 1:
+        print("Congrats! You win")
+        return_to_first_page()
+    elif winner == 2:
+        print("Sorry, you lost")
+        return_to_first_page()
+    else:
+        print("Draw! No one won")
+        return_to_first_page()
 
     print_grid(grid)
 
-if winner == 1:
-    print("Congrats! You win")
-elif winner == 2:
-   print("Sorry, you lost")
-else:
-   print("Draw! No one won")
-
-print_grid(grid)
+play_game()
